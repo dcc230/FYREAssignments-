43e3dfc82ec01076dc10e8de116567439e7fbf97
Dean Cunningham <dcc230@lehigh.edu>
2026-09-09 15:53:28 -0400

    Add files via upload

---
+++ Programming Activities/Program4.py
@@ -0,0 +1,18 @@
+# This program was created in Arduino Lab for MicroPython
+#Blinking Program
+
+
+#incorperate Modules
+import machine #microcontroller stuff
+import time 
+
+#make LED object
+led = machine.Pin(0,machine.Pin.OUT)
+#infinite Loop
+while True:
+  led.value(1) #led on
+  time.sleep(.5)
+  led.value(0)
+  
+
+  
---
+++ Programming Activities/whopping_baboon.py
@@ -0,0 +1,4 @@
+# This program was created in Arduino Lab for MicroPython
+#variable to store my name
+name = "Dean Cunningham"
+print(name) # Output the contents of the variable "name" to the REPL
